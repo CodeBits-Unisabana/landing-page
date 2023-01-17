ec550bef85a20d34be5cc9c2d845e87830617fed
Richard Guevara <leonvilla6@outlook.com>
2023-01-17 02:25:29 -0500

    Add metadata

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,15 @@ export default function Head() {
     <>
       <title>CodeBits</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Somos CodeBits, un grupo de estudiantes apasionados por el desarrollo de software quienes buscamos enseñar y apoyar a otros estudiantes a adquirir nuevas habilidades tecnicas y brindarles la oportunidad de participar en proyectos reales. "
+      />
+      <meta
+        name="keywords"
+        content="CodeBits, consultoria, voluntario, desarrollo de software, capacitaciones, talleres, front end, back end, diseno ux/ui"
+      />
+      <meta name="author" content="CodeBits" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
